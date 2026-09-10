9b87ce16ac99f99827c2a8c31bf271edc02f9be2
Arnaud <arnaud.tortora@gmail.com>
2026-09-10 02:10:05 +0200

    add themes feature

---
+++ features/themes/actions.ts
@@ -0,0 +1,19 @@
+"use server";
+
+import { prisma } from "@/lib/prisma";
+import { Theme } from "@prisma/client";
+
+/**
+ * Get the list of available themes sorted by name
+ *
+ * @returns Theme list, sorted by name (A → Z).
+ */
+const getThemes = async (): Promise<Theme[]>  => {
+  return await prisma.theme.findMany({
+    orderBy: {
+      name: "asc"
+    }
+  })
+}
+
+export { getThemes };
