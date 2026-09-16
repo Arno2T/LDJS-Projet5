aa261e2da40ef1004f2eaf0a99e17e34258a2ba6
Arno2T <arnaud.tortora@gmail.com>
2026-09-16 16:18:27 +0200

    Add page Articles (#6)

---
+++ features/articles/actions.ts
@@ -0,0 +1,22 @@
+"use server";
+
+import { requireAuth } from "@/lib/auth/session";
+import { prisma } from "@/lib/prisma";
+import { Article } from "@prisma/client";
+import { getSubscriptionsByUser } from "../subscriptions/actions";
+
+const getArticles = async () => {
+  await requireAuth();
+  const subscriptions = await getSubscriptionsByUser();
+
+  const themeIds = subscriptions.map((sub) => sub.themeId);
+
+  return prisma.article.findMany({
+    where: { themeId: { in: themeIds } },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+};
+
+export { getArticles };
